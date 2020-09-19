f71769d5a34e6f782ac14647c1969bdf7ef9baa1
Takuma-Kawa <71553318+Takuma-Kawa@users.noreply.github.com>
2020-09-19 22:34:01 +0900

    Create test.java

---
+++ test.java
@@ -0,0 +1,77 @@
+﻿class Account{
+  
+
+private String name ;
+   
+private String no;
+   
+private long balance;
+   
+private Day kaisetu;
+
+   
+ 
+Account(String n,String num,long z,Day kaisetu){
+	
+name=n;
+	no=num;
+	balance=z;
+	this.kaisetu=new Day(kaisetu);
+
+	
+    }
+   
+
+public Day getkaisetu(){
+	
+return new Day(kaisetu);}
+
+    
+
+    
+String getName(){
+	return name;
+ 
+   }
+
+   
+String getNo(){
+	return no;
+  
+  }
+
+    
+long getBalance(){
+	return balance;
+  
+  }
+
+ 
+  
+void deposit(long k){
+	balance += k;
+
+ 
+   }
+
+    
+void withdraw(long k){
+	balance -=k;
+  
+  }
+   
+
+public void putspec(){
+
+	System.out.println("名前:"+ n);
+	
+　　　　System.out.println("番号:"+num);
+
+	System.out.println("預金残高:"+z);
+
+
+   
+ }
+
+
+}
